bda0f214a96e2b896a1a2557ee3077d0db40a865
Divyanshu <shukladivyanshu967@gmail.com>
2018-07-10 05:03:29 +0530

    refreshing fragment

---
+++ app/src/main/java/com/kalabhedia/gimme/BroadCastReceiver.java
@@ -0,0 +1,13 @@
+package com.kalabhedia.gimme;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadCastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
